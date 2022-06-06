b0564f2c5af48e1c462f69d4359c3ec97fa3b929
neeraj <neeraj.ak2995@gmail.com>
2022-06-07 00:39:18 +0530

    test for resolving conflict

--- cache_test/src/com/CacheTest1.java
+++ cache_test/src/com/CacheTest1.java
@@ -4,7 +4,6 @@ public class CacheTest1 {
 
 	public static void main(String[] args) {
 		
-		// hiii
 		       // 1. initiate the cache with capacity 5 
 				TimeAndSizeBasedEvictionCache<String, String> cache = new TimeAndSizeBasedEvictionCache<String, String>(5,1);
 
