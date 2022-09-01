42fa8c96cd04799b86dd7c6438786711dd469dbb
neeraj <neeraj.ak2995@gmail.com>
2022-09-01 14:41:36 +0530

    changes in branch _2 for test conflict

--- cache_test/src/com/GitPractice.java
+++ cache_test/src/com/GitPractice.java
@@ -5,6 +5,7 @@ public class GitPractice {
 	public static void main(String[] args) {
 		System.out.println("this is for revert purpose ");
 		System.out.println("branch_2 created");
+		System.out.println("changes in branch_2 ");
 	}
 
 }
